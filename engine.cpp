ff9a7750871a9acea7971cc4f171ec0fcfafd2c7
Toksisitee <loading.buzzz@gmail.com>
2019-06-03 00:43:18 +0300

    PACKETTYPE_CREATE_MARKER

--- engine.cpp
+++ engine.cpp
@@ -2673,6 +2673,17 @@ skip:;
 				Markers[MarkerSelected].ex = cx;
 				Markers[MarkerSelected].ez = cz;
 				Markers[MarkerSelected].ey = cy;
+
+				if (net.IsInitialized())
+				{
+					struct Packet *p = new Packet;
+					p->wType = PACKETTYPE_CREATE_MARKER;
+					p->wData[0] = MarkerSelected;
+					p->wData[1] = (WORD)(Markers[MarkerSelected].x - 0.5f);
+					p->wData[2] = (WORD)(Markers[MarkerSelected].z - 0.5f);
+					net.SendPacket(p);
+					p->del();
+				}
 			}
 
 			DlgMarkersUpdate(hDlgMarkers);
